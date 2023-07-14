a054f2351f208a6bc368f129bcc10fc66360a84b
Laszlo Olasz <highgfx@gmail.com>
2023-07-14 14:23:54 +0300

    tests

--- Shared/Data/Models/Patient.cs
+++ Shared/Data/Models/Patient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +10,8 @@ namespace BlazorApp.Shared.Data.Models
 {
     public class Patient
     {
-        public int? Id { get; set; }
+        [Key]
+        public int? Id { get; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
