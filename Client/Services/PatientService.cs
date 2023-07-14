db27245f852b40f3a7b01ead417917ae97b27132
Laszlo Olasz <highgfx@gmail.com>
2023-07-14 18:33:17 +0300

    mopdified endpoints

--- Client/Services/PatientService.cs
+++ Client/Services/PatientService.cs
@@ -14,28 +14,28 @@ namespace BlazorApp.Client.Services
 
         public async Task<PatientModel[]> GetPatients()
         {
-            return await _httpClient.GetFromJsonAsync<PatientModel[]>("api/patient");
+            return await _httpClient.GetFromJsonAsync<PatientModel[]>("data-api/rest/Patient");
         }
 
         public async Task<PatientModel> GetPatient(int id)
         {
-            return await _httpClient.GetFromJsonAsync<PatientModel>($"api/patient/{id}");
+            return await _httpClient.GetFromJsonAsync<PatientModel>($"data-api/rest/Patient/Id/{id}");
         }
 
         public async Task<PatientModel> AddPatient(PatientModel patient)
         {
-            var response = await _httpClient.PostAsJsonAsync("api/patient", patient);
+            var response = await _httpClient.PostAsJsonAsync("data-api/rest/Patient/", patient);
             return await response.Content.ReadFromJsonAsync<PatientModel>();
         }
 
         public async Task UpdatePatient(PatientModel patient)
         {
-            await _httpClient.PutAsJsonAsync($"api/patient/{patient.Id}", patient);
+            await _httpClient.PutAsJsonAsync($"data-api/rest/Patient/Id/{patient.Id}", patient);
         }
 
         public async Task DeletePatient(int id)
         {
-            await _httpClient.DeleteAsync($"api/patient/{id}");
+            await _httpClient.DeleteAsync($"data-api/rest/Patient/Id/{id}");
         }
 
     }
