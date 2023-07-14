583f5ffdae52c84de7d2da534c28cc2fda072d3c
Laszlo Olasz <highgfx@gmail.com>
2023-07-14 11:47:02 +0300

    added database configuration

--- Shared/Data/Models/Doctor.cs
+++
@@ -1,8 +0,0 @@
-﻿public class Doctor
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Specialization { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-}
